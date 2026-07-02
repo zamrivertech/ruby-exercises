08d6074f3b3aa7c490cbe5a223a9efe182abb8c6
zamrivertech <mohamadtasul8000@gmail.com>
2026-07-02 09:38:21 +0200

    Solve and pass returns result with a string and integer

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -2,6 +2,7 @@ def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
+  {:color => color, :number => number}
 end
 
 def favorite_color(favorite_list)
